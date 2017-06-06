9c36a234aedf847f71f62765370d66f13ed0bbdc
Marilyn Carlin and kimlan nguyen <marilyn+kimlan@>
2017-06-06 14:36:37 -0700

    Add animals_form.cshtml, animals.cshtml, species_form.cshtml. s;ecies.cshtml, success,cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,8 +9,32 @@ namespace animalShelter
     public HomeModule()
     {
       Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/animals"] = _ => {
+        List<Animal> AllAnimals = Animal.GetAll();
+        return View["animals.cshtml", AllAnimals];
+      };
+      Get["/species"] = _ => {
         List<Species> AllSpecies = Species.GetAll();
-        return View["index.cshtml", AllSpecies];
+        return View["species.cshtml", AllSpecies];
+      };
+      Get["/species/new"] = _ => {
+        return View["species_form.cshtml"];
+      };
+      Post["/species/new"] = _ => {
+        Species newSpecies = new Species(Request.Form["species-name"]);
+        newSpecies.Save();
+        return View["success.cshtml"];
+      };
+      Get["/animals/new"] = _ => {
+        List<Species> AllSpecies = Species.GetAll();
+        return View["animals_form.cshtml", AllSpecies];
+      };
+      Post["/animals/new"] = _ => {
+        Animal newAnimal = new Animal(Request.Form["animal-name"], Request.Form["animal-gender"], Request.Form["animal-breed"],Request.Form["animal-date"], Request.Form["species-id"]);
+        newAnimal.Save();
+        return View["success.cshtml"];
       };
     }
   }
